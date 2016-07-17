599a2a35c5e6f70b4c41a4393db93fa37b48526b
Joe Stevens <joseph.stevens34@gmail.com>
2016-07-17 01:02:31 -0400

    Added new conversion controller linked view to controller

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Joe Stevens on 7/17/16.
+//  Copyright © 2016 Joe Stevens. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+}
