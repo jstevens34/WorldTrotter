81d864ad8dd8f04cd5ed5f19465128914fcb8b04
Joe Stevens <joseph.stevens34@gmail.com>
2016-07-20 20:56:48 -0400

    Progrommatic constraints

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,17 +7,36 @@
 //
 
 import UIKit
+import MapKit
+
 
 class MapViewController: UIViewController{
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        //Create MapView 
+        mapView = MKMapView()
+        
+        //Set it as *the* view of this view controller
+        view = mapView
+        
+        let segmentedControl
+            = UISegmentedControl(items: ["Standard", "Hybrid", "Satelite"])
+        segmentedControl.backgroundColor
+            = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+    }
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("MapViewController loaded its view")
-    
-    
-        
         
     }
     
